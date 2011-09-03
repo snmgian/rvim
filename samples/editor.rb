3dcda5dff2d559647e23231469686ad1e8e9a59b
gian <snmgian@gmail.com>
2011-09-03 15:46:23 -0300

    support for CR and multiple lines

--- samples/editor.rb
+++ samples/editor.rb
@@ -1,22 +1,39 @@
 #!/usr/bin/env ruby -w
 
 require 'curses'
+require_relative '../lib/rvim/logger.rb'
+
+RVim::Logger.init
 
 Curses.init_screen
 Curses.noecho
-#Curses.crmode
 
-Curses.addstr("Hello World!");
-Curses.refresh
+x, y = 0, 0
+Curses.setpos(y, x)
+cols, lines = Curses.cols, Curses.lines
 
 loop do
-  case ch = Curses.getch
+  ch = Curses.getch
+  $log.debug "getch: #{ch.class}, #{ch.inspect}"
+
+  case ch
   when ?q
     break
+  when 10 # <Enter>
+    Curses.insch "\n"
+    x = 0
+    y += 1
+    Curses.setpos(y, x)
   else
     Curses.insch ch
+    Curses.doupdate
+    x += 1
+    if x >= cols
+      x = 0
+      y += 1
+    end
+    Curses.setpos(y, x)
   end
-  #sleep 0.1
 end
 
 Curses.close_screen
